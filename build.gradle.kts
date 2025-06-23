765c74583a9eb2fc8f5ae1062bcdad08b9ef01fd
mooncrown04 <mooncrown04@gmail.com>
2025-06-23 16:18:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/mooncrown04/mooncrown35")
 
-        authors = listOf("keyiflerolsun")
+        authors = listOf("sarapcanagii")
     }
 
     android {
