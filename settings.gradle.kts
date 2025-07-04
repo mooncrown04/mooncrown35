7e76073c0a48c0a25863615d8a946f0258e86491
mooncrown04 <mooncrown04@gmail.com>
2025-07-05 00:29:58 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+// This file sets what projects are included.
+// All new projects should get automatically included unless specified in the "disabled" variable.
 
-val disabled = listOf<String>("__Temel")
+val disabled = listOf<String>()
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
@@ -13,7 +14,6 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
-
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
 // include("PluginName")
