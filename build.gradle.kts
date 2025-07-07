e7215b61f8f469ae3c74e95d03b8abb52b91eb03
mooncrown04 <mooncrown04@gmail.com>
2025-07-07 17:09:27 +0300

    İlk yükleme: CloudStream M3U eklentisi

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    id("java-library")
+}
+
+group = "com.mooncrown04"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:3.1.2")
+}
+
+tasks.register<Jar>("cs3") {
+    archiveBaseName.set("mooncrown35")
+    archiveExtension.set("cs3")
+    from(sourceSets.main.get().output)
+    from("plugin.json")
+}
