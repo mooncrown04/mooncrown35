0fbf1d3ae337fbe4c2c1181c1fd60df9ebd349d2
mooncrown04 <mooncrown04@gmail.com>
2025-07-07 22:37:33 +0300

    Update build.gradle.kts

--- m3u/build.gradle.kts
+++ m3u/build.gradle.kts
@@ -1,23 +1,37 @@
-// Use an integer for version numbers
+plugins {
+    kotlin("jvm") version "1.9.10"
+}
+
 version = 2
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
     description = "Watch livestreams from Twitch"
     authors = listOf("mooncrown04")
 
     /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     **/
+    status = 1
 
     tvTypes = listOf("Live")
     iconUrl = "https://www.google.com/s2/favicons?domain=twitch.tv&sz=%size%"
-
     isCrossPlatform = true
+}
+
+repositories {
+    mavenCentral()
+    google()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.github.recloudstream.cloudstream:library-jvm:master-SNAPSHOT")
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 }
