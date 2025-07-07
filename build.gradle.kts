54454dae22297df8ad9b4477b11f64ddd8c2dfc5
mooncrown04 <mooncrown04@gmail.com>
2025-07-07 22:45:35 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,91 +1,28 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
-buildscript {
- 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.8.0"
+    application
 }
+
+group = "com.mooncrown04"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
-    google()
-    maven { url = uri("https://jitpack.io") } // Burası şart
+    maven("https://jitpack.io")
 }
-
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("com.github.recloudstream.cloudstream:library-jvm:master-SNAPSHOT")
-    implementation("com.github.Blatzar:NiceHttp:0.4.11")
-    implementation("org.jsoup:jsoup:1.18.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-}
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/mooncrown04/mooncrown35")
-    }
-
-    android {
-        namespace = "com.mooncrown04"
-
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8) // Required
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
-                )
-            }
-        }
-    }
-
-dependencies {
-    val implementation by configurations
-
-    implementation("com.github.recloudstream:cloudstream:3.0.0")
-
     implementation(kotlin("stdlib"))
-    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("com.github.recloudstream.cloudstream:library-jvm:master-SNAPSHOT")
     implementation("org.jsoup:jsoup:1.18.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 }
+
+application {
+    mainClass.set("com.mooncrown04.M3UStreamProviderKt") // Eğer main fonksiyon yoksa bu kısmı kaldırabilirsin
 }
 
-task<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
+
