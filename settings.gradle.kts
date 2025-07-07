830722c32d3dbe7e43f65b768ab243c829db00c7
mooncrown04 <mooncrown04@gmail.com>
2025-07-07 23:02:58 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
-rootProject.name = "mooncrown35"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://recloudstream.github.io/maven")
+    }
 
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "cloudstream-plugin") {
+                useModule("com.github.recloudstream.cloudstream:gradle:master-SNAPSHOT")
+            }
+        }
+    }
+}
+
+rootProject.name = "mooncrown35"
 include(":m3u")
