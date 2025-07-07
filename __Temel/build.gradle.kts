ed8a3c4122a70aedcc2dfcf5f48aa8fad020fb45
mooncrown04 <mooncrown04@gmail.com>
2025-07-07 22:21:55 +0300

    Delete __Temel/build.gradle.kts

--- __Temel/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 0
-
-cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/mooncrown04/mooncrown35")
-
-        authors = listOf("mooncrown04")
-    }
-android {
-        namespace = "com.mooncrown04"
